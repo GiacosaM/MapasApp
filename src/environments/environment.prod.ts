31a2a103083ad62c02f1c0cce1b78291d4dbbdc2
GiacosaM <martin_giacosa@hotmail.com>
2023-03-27 08:36:45 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoibWFydGluZ2lhY29zYSIsImEiOiJjbGZrNzNmM3AwN3lpM3dveHpncWdscmQ5In0.Dg5kBRriXGlMckje1wTCyg',
 };
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapBoxToken: 'pk.eyJ1IjoibWFydGluZ2lhY29zYSIsImEiOiJjbGZrNzNmM3AwN3lpM3dveHpncWdscmQ5In0.Dg5kBRriXGlMckje1wTCyg',
 };
 
 /*
